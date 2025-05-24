daed22c87859def00ca0e9a95d3b1a15237d1c4a
scorbiclife <scorbiclife+git@gmail.com>
2025-05-24 17:17:26 +0900

    refactor: simplify and reorder test cases

--- src/authn/index.test.js
+++ src/authn/index.test.js
@@ -29,7 +29,20 @@ app.use(
 );
 
 describe("authentication", () => {
-  test("user registration succeeds on valid request", async () => {
+  test("on request without user info, responds with 400", async () => {
+    const response = await supertest(app)
+      .post("/")
+      .send({
+        user: {
+          username: "Jacob",
+          email: "jake@jake.jake",
+        },
+      })
+      .set("Accept", "application/json");
+    expect(response.status).toStrictEqual(400);
+  });
+
+  test("on valid user login, responds with 200 with the expected return type", async () => {
     const response = await supertest(app)
       .post("/")
       .send({
@@ -48,38 +61,10 @@ describe("authentication", () => {
       bio: "",
       image: null,
     });
-  });
-
-  test("on valid request, server signs a valid JWT token", async () => {
-    const response = await supertest(app)
-      .post("/")
-      .send({
-        user: {
-          username: "Jacob",
-          email: "jake@jake.jake",
-          password: "jakejake",
-        },
-      })
-      .set("Accept", "application/json")
-      .expect(200)
-      .expect("Content-Type", /json/);
     const token = response?.body?.user?.token;
     expect(typeof token).toStrictEqual("string");
     await expect(testJwtService.verify(token)).resolves.toMatchObject({
       email: "jake@jake.jake",
     });
-  });
-
-  test("user registration fails on invalid request", async () => {
-    const response = await supertest(app)
-      .post("/")
-      .send({
-        user: {
-          username: "Jacob",
-          email: "jake@jake.jake",
-        },
-      })
-      .set("Accept", "application/json");
-    expect(response.status).toStrictEqual(400);
   });
 });
