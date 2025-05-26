c5d51473eaa678d8d2b81e082037f931a9109714
scorbiclife <scorbiclife+git@gmail.com>
2025-05-26 12:09:09 +0900

    fix: set password_hash in user table to be not null

--- migrations/20250526004429_create-users-table.up.sql
+++ migrations/20250526004429_create-users-table.up.sql
@@ -1,7 +1,7 @@
 create table `user` (
     id bigint not null auto_increment,
     username varchar(64) not null,
-    password_hash binary(60),  -- assuming bcrypt (60 octets)
+    password_hash binary(60) not null,  -- assuming bcrypt (60 octets)
     created_at datetime not null default now(),
     updated_at datetime not null default now(),
     deleted_at datetime default null,
