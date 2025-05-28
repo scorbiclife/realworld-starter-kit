178ba9df2d711e3cc0d7d16b3128bf43b32c048e
scorbiclife <scorbiclife+git@gmail.com>
2025-05-28 09:04:10 +0900

    feat: create transaction wrappers

---
+++ src/db/mysqlTransaction.js
@@ -0,0 +1,59 @@
+export class MysqlTransactionWrapper {
+  /**
+   * We save the connection instead of the pool to guarantee
+   * that the statements are executed on the same connection
+   * inside a transaction.
+   *
+   * @param {object} param0
+   * @property {Connection} param0.connection
+   */
+  constructor({ connection }) {
+    this.connection = connection;
+  }
+
+  /**
+   * Wrap the given action inside a transaction.
+   *
+   * @template {any[]} Args
+   * @template {unknown} Result
+   * @template {unknown} Error
+   * @param {(connection: import("mysql2/promise").Connection, ...args: Args) => Promise<Result, Error>} action
+   * @param {Args} args
+   * @returns {Promise<Result, Error>}
+   */
+  async transactionallyRun(action, ...args) {
+    this.connection.query("START TRANSACTION;");
+    let result;
+    try {
+      result = await action(this.connection, ...args);
+    } catch (error) {
+      this.connection.query("ROLLBACK;");
+      throw error;
+    }
+    this.connection.query("COMMIT;");
+    return result;
+  }
+
+  /**
+   * Wrap the given action inside a transaction with rollback.
+   *
+   * @template {any[]} Args
+   * @template {unknown} Result
+   * @template {unknown} Error
+   * @param {(connection: import("mysql2/promise").Connection, ...args: Args) => Promise<Result, Error>} action
+   * @param {Args} args
+   * @returns {Promise<Result, Error>}
+   */
+  async withAutoRollbackRun(action, ...args) {
+    this.connection.query("START TRANSACTION;");
+    let result;
+    try {
+      result = await action(this.connection, ...args);
+    } catch (error) {
+      throw error;
+    } finally {
+      this.connection.query("ROLLBACK;");
+    }
+    return result;
+  }
+}
---
+++ src/db/mysqlTransaction.test.js
@@ -0,0 +1,101 @@
+import {
+  describe,
+  test,
+  expect,
+  afterAll,
+  beforeAll,
+  beforeEach,
+  afterEach,
+} from "@jest/globals";
+import mysql from "mysql2/promise";
+
+import { connectionSettings } from "#src/db/connectionSettings.js";
+
+import { MysqlTransactionWrapper } from "./mysqlTransaction.js";
+
+let connection;
+let wrapper;
+
+beforeAll(async () => {
+  connection = await mysql.createConnection(connectionSettings);
+  wrapper = new MysqlTransactionWrapper({
+    connection,
+  });
+});
+
+afterAll(async () => {
+  await wrapper.connection.destroy();
+});
+
+beforeEach(async () => {
+  await connection.query("DROP TABLE IF EXISTS temporary_table");
+  await connection.query("DROP TABLE IF EXISTS nonexistent_table;");
+  await connection.query(
+    "CREATE TABLE temporary_table(id bigint not null auto_increment, name varchar(64) not null, primary key (id));"
+  );
+});
+
+afterEach(async () => {
+  await connection.query("DROP TABLE temporary_table;");
+});
+
+/**
+ * Note: we are using DMLs for testing transactions because
+ *  [MySQL implicitly commits DDL statements even in a transaction](https://stackoverflow.com/questions/4692690/is-it-possible-to-roll-back-create-table-and-alter-table-statements-in-major-sql)
+ */
+
+describe("MysqlTransactionWrapper", () => {
+  test("`commits a successful transaction", async () => {
+    await wrapper.transactionallyRun(async (connection) => {
+      connection.query("INSERT INTO temporary_table(name) VALUES (('foo'));");
+    });
+    const [result] = await connection.query("SELECT * from temporary_table");
+    expect(result.length).toBe(1);
+  });
+
+  test("rolls back and throws on an unsuccessful transaction", async () => {
+    try {
+      await wrapper.transactionallyRun(async (connection) => {
+        await connection.query("INSERT INTO temporary_table VALUES (('foo'));");
+        const result = await connection.query(
+          "SELECT * FROM nonexistent_table;"
+        );
+        return result;
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      const [result] = await connection.query("SELECT * from temporary_table");
+      expect(result.length).toBe(0);
+      return;
+    }
+    throw new Error("Unreachable");
+  });
+});
+
+describe("MysqlTransactionWrapper.prototype.withRollbackRun", () => {
+  test("rolls back a successful transaction but does not throw", async () => {
+    await wrapper.withAutoRollbackRun(async (connection) => {
+      connection.query("INSERT INTO temporary_table(name) VALUES (('foo'));");
+    });
+    const [result] = await connection.query("SELECT * from temporary_table");
+    expect(result.length).toBe(0);
+  });
+
+  test("rolls back and throws on an unsuccessful transaction", async () => {
+    try {
+      await wrapper.withAutoRollbackRun(async (connection) => {
+        await connection.query("INSERT INTO temporary_table VALUES (('foo'));");
+        const result = await connection.query(
+          "SELECT * FROM nonexistent_table;"
+        );
+        return result;
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      const [result] = await connection.query("SELECT * from temporary_table");
+      expect(result.length).toBe(0);
+      return;
+    }
+    throw new Error("Unreachable");
+  });
+});
