0942048f4a9b700c5eac1a6c61e7c9578af20dd3
scorbiclife <scorbiclife+git@gmail.com>
2025-05-28 14:25:48 +0900

    feat: MysqlBcryptUserRepository

---
+++ src/authn/MysqlBcryptUserRepository.js
@@ -0,0 +1,31 @@
+export class MysqlBcryptUserRepository {
+  /**
+   *
+   * @param {import("mysql2/promise").Connection} connection
+   * @param {{username: string, email: string, password_hash: string}} param1
+   * @returns {Promise<{ status: "success", id: number } | { status: "duplicate_user"}>}
+   * @throws - unexpected errors
+   */
+  async save(connection, { username, email, password_hash }) {
+    let results;
+    let rows;
+    try {
+      [results, rows] = await connection.execute(
+        "insert into `user` (username, email, password_hash) values (?, ?, ?);",
+        [username, email, password_hash]
+      );
+    } catch (error) {
+      if (error.code === "ER_DUP_ENTRY") {
+        // case: user already exists
+        return { status: "duplicate_user" };
+      }
+      throw error;
+    }
+    // @ts-ignore
+    const [[{ id }], _] = await connection.execute(
+      "select id from `user` where email = ?;",
+      [email]
+    );
+    return { status: "success", id };
+  }
+}
---
+++ src/authn/MysqlBcryptUserRepository.test.js
@@ -0,0 +1,52 @@
+import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import mysql from "mysql2/promise";
+
+import { connectionSettings } from "#src/db/connectionSettings.js";
+
+import { MysqlBcryptUserRepository } from "./MysqlBcryptUserRepository.js";
+import { MysqlTransactionWrapper } from "#src/db/mysqlTransaction.js";
+
+describe(MysqlBcryptUserRepository.name, () => {
+  let tx;
+
+  beforeAll(async () => {
+    const connection = await mysql.createConnection(connectionSettings);
+    tx = new MysqlTransactionWrapper({ connection });
+  });
+
+  afterAll(async () => {
+    tx.connection.close();
+  });
+
+  const repo = new MysqlBcryptUserRepository();
+
+  describe(MysqlBcryptUserRepository.prototype.save.name, () => {
+    test("succeeds on happy case", async () => {
+      const { status, id } = await tx.autoRollback(repo.save.bind(repo), {
+        username: "foo",
+        email: "foo@fooverse.com",
+        password_hash: "asdf",
+      });
+      expect(status).toBe("success");
+      expect(typeof id).toBe("number");
+    });
+
+    test("fails with status `duplicate_user` on duplicate email", async () => {
+      await tx.autoRollback(async (connection) => {
+        // This should pass
+        await repo.save(connection, {
+          username: "foo",
+          email: "foo@fooverse.com",
+          password_hash: "asdf",
+        });
+        // This should not throw because duplicate user is an expected condition
+        const { status } = await repo.save(connection, {
+          username: "bar",
+          email: "foo@fooverse.com",
+          password_hash: "qwerty",
+        });
+        expect(status).toBe("duplicate_user");
+      });
+    });
+  });
+});
