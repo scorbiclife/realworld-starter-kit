0a9ea4a22ee41a7af4c0ffe40d67eb01ccfb735b
scorbiclife <scorbiclife+git@gmail.com>
2025-06-02 13:58:15 +0900

    fix: broken app router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { authnRouter } from "#src/authn/index.js";
+
+import { makeRouter as makeAuthnRouter } from "#src/authn/index.js";
+import { Hs256JwtService } from "./authn/JwtService.js";
+import { InMemoryBcryptUserService } from "./authn/user/InMemoryBcryptUserService.js";
+
+const jwtService = new Hs256JwtService({
+  secret: process.env.JWT_SECRET,
+  expiresIn: "10m",
+});
+
+const userService = new InMemoryBcryptUserService({});
+
+const authnRouter = makeAuthnRouter({
+  jwtService,
+  userService,
+});
 
 export const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/api/users", authnRouter);
-
-app.get("/", (req, res) => {
-  res.json("Hello World!");
+app.use("/", (req, res) => {
+  res.json({ message: "hello world" });
+  return;
 });
