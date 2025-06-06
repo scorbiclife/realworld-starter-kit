45dc71b6c82f77d6f285c2576f2eae095212b241
scorbiclife <scorbiclife+git@gmail.com>
2025-06-06 09:52:12 +0900

    refactor: extract connection-dependent logic to helper method

--- src/authn/user/MysqlBcryptUserService.js
+++ src/authn/user/MysqlBcryptUserService.js
@@ -20,25 +20,37 @@ export class MysqlBcryptUserService extends UserService {
 
   async register({ username, email, password }) {
     const connection = await this.connectionPool.getConnection();
-    const user = await BcryptUser.fromJson({ username, email, password });
-    const action = async (connection) => {
-      return await this.repository.save(connection, {
-        username,
-        email,
-        password_hash: user.passwordHash,
-      });
-    };
     try {
-      const result = await this.transactionRunner(action, connection);
-      if (result.success) {
-        return /** @type {const} */ ({ success: true, user });
-      }
-      if (result.status === "duplicate_user") {
-        return /** @type {const} */ ({ success: false, code: "EXISTING_USER" });
-      }
-      throw new Error("Unreachable");
+      return await this.transactionRunner(
+        this.userRegistrationTransaction,
+        connection,
+        { username, email, password }
+      );
     } finally {
       connection.release();
     }
   }
+
+  /**
+   * Used an arrow function for easier use in transaction wrappers that accept a free function.
+   * @param {import("mysql2/promise").Connection} connection
+   */
+  userRegistrationTransaction = async (
+    connection,
+    { username, email, password }
+  ) => {
+    const user = await BcryptUser.fromJson({ username, email, password });
+    const result = await this.repository.save(connection, {
+      username,
+      email,
+      password_hash: user.passwordHash,
+    });
+    if (result.success) {
+      return /** @type {const} */ ({ success: true, user });
+    }
+    if (result.status === "duplicate_user") {
+      return /** @type {const} */ ({ success: false, code: "EXISTING_USER" });
+    }
+    throw new Error("Unreachable");
+  };
 }
--- src/db/transaction.d.ts
+++ src/db/transaction.d.ts
@@ -1,3 +1,5 @@
+import { Connection as MysqlConnection } from "mysql2/promise";
+
 export type TransactionRunner = <A extends any[], R, E>(
   action: (connection: MysqlConnection, ...args: A) => Promise<R, E>,
   connection: MysqlConnection,
